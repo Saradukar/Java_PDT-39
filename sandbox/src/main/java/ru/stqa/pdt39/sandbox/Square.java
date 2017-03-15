9a28d3b2efd22cb401ad08f1a55e67850c57628c
Saradukar <borealis@yandex.ru>
2017-03-16 01:57:32 +0300

    Первая часть пройдена

---
+++ sandbox/src/main/java/ru/stqa/pdt39/sandbox/Square.java
@@ -0,0 +1,20 @@
+package ru.stqa.pdt39.sandbox;
+
+/**
+ * Created by Borealis on 15.03.2017.
+ */
+public class Square {
+
+  public double l;
+
+  public Square(double l) {
+    this.l = l;
+  }
+
+  public double area() {
+    return this.l * this.l;
+  }
+
+
+
+}
