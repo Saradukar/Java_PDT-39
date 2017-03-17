d00d511b8afa454ac2765e4e09a274decb5ae410
Saradukar <borealis@yandex.ru>
2017-03-17 15:46:58 +0300

    Выполнены п. 1 и п. 2 задания 2
    Создан класс Point, создана функция distance(Point p1, Point p2). В
    таком виде программа работает

---
+++ sandbox/src/main/java/ru/stqa/pdt39/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pdt39.sandbox;
+
+/**
+ * Created by Borealis on 17.03.2017.
+ */
+public class Point {
+
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+}
